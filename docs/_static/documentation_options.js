8f28036f3e11f131c00aee3997ee13d8cfc0e691
Chapin Bryce <27cbryce@gmail.com>
2019-11-26 17:57:34 -0500

    Added windows makefile script and updated to include GA tracking.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20191103',
+    VERSION: '20191126',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
